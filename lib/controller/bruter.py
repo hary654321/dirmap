0b85725e4c917c16be3ae6322b68ebaefb97f64d
wangrui <hary654321@126.com>
2023-03-31 17:52:27 +0800

    1

--- lib/controller/bruter.py
+++ lib/controller/bruter.py
@@ -451,7 +451,7 @@ def responseHandler(response):
         outputscreen.info('\r'+msg+' '*(th.console_width-len(msg)+1))
         #已去重复，结果保存。NOTE:此处使用response.url进行文件名构造，解决使用-iL参数时，不能按照域名来命名文件名的问题
         #使用replace()，替换`:`，修复window下不能创建有`:`的文件问题
-        saveResults(msg)
+        saveResultsAppend(msg)
     #关于递归扫描。响应在自定义状态码中时，添加判断是否进行递归扫描
     if response.status_code in conf.recursive_status_code:
         if conf.recursive_scan:
